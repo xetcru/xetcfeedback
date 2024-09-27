39c7eef33ae5de3d413cf060d021a3a8a93a7326
Sergey <37002842+xetcru@users.noreply.github.com>
2024-09-28 02:53:18 +0300

    Create .description.php

---
+++ .description.php
@@ -0,0 +1,12 @@
+<?
+if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true) die();
+
+$arComponentDescription = array(
+    "NAME" => "Xetc Send Error",
+    "DESCRIPTION" => "Xetc send error component",
+    "ICON" => "/images/feedback.gif",
+    "PATH" => array(
+        "ID" => "utility",
+    ),
+);
+?>
